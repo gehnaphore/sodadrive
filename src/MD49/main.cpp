ca2d63f7ac226b4bf309025d5f9a9864b149ad67
Alexander Entinger <cto@lxrobotics.com>
2016-08-04 07:34:08 +0200

    Refactoring on MD49 regulator loop for better readability

--- src/MD49/main.cpp
+++ src/MD49/main.cpp
@@ -31,9 +31,30 @@ static double const T_LOOP_UPDATE_s  = 0.1;   /* 100 ms */
  * PROTOTYPES
  **************************************************************************************/
 
-void speed_1_callback     (std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator );
-void speed_2_callback     (std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator );
-void getDeltaEncoderValues(MD49 &md49, uint32_t &delta_encoder_1,   uint32_t &delta_encoder_2     );
+void speed_1_callback      (std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator );
+void speed_2_callback      (std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator );
+
+void getDeltaEncoderValues    (MD49          &      md49,
+                               uint32_t      &      delta_encoder_1,
+                               uint32_t      &      delta_encoder_2);
+
+void getActualSpeed_m_per_s   (uint32_t      const  delta_encoder_1,
+                               uint32_t      const  delta_encoder_2,
+                               double        &      actual_speed_1_m_per_s,
+                               double        &      actual_speed_2_m_per_s);
+
+void getSpeedFromRegulator    (MD49Regulator const &md49_regulator,
+                               int8_t        &      speed_1,
+                               int8_t        &      speed_2);
+
+void setSpeedAtMotorController(MD49          &      md49,
+                               int8_t        const  speed_1,
+                               int8_t        const  speed_2);
+
+void publishActualSpeed       (ros::Publisher&      actual_speed_1_publisher,
+                               double        const  actual_speed_1_m_per_s,
+                               ros::Publisher&      actual_speed_2_publisher,
+                               double        const  actual_speed_2_m_per_s);
 
 /**************************************************************************************
  * MAIN
@@ -64,27 +85,41 @@ int main(int argc, char **argv)
 
   while(ros::ok())
   {
+    ros::spinOnce();
+
     uint32_t delta_encoder_1 = 0,
              delta_encoder_2 = 0;
 
-    getDeltaEncoderValues(md49, delta_encoder_1, delta_encoder_2);
+    getDeltaEncoderValues    (md49,
+                              delta_encoder_1,
+                              delta_encoder_2);
 
-    double const actual_speed_1_m_per_s = EMG49::calcDistanceTraveled_m(delta_encoder_1, WHEEL_DIAMETER_m) / T_LOOP_UPDATE_s;
-    double const actual_speed_2_m_per_s = EMG49::calcDistanceTraveled_m(delta_encoder_2, WHEEL_DIAMETER_m) / T_LOOP_UPDATE_s;
+    double actual_speed_1_m_per_s = 0.0,
+           actual_speed_2_m_per_s = 0.0;
+
+    getActualSpeed_m_per_s   (delta_encoder_1,
+                              delta_encoder_2,
+                              actual_speed_1_m_per_s,
+                              actual_speed_2_m_per_s);
 
     md49_regulator.updateWithActualValue(actual_speed_1_m_per_s, actual_speed_2_m_per_s);
 
-    int8_t const speed_1 = md49_regulator.getSpeed1();
-    int8_t const speed_2 = md49_regulator.getSpeed2();
+    int8_t speed_1 = 0,
+           speed_2 = 0;
 
-    md49.setSpeed1(speed_1);
-    md49.setSpeed2(speed_2);
+    getSpeedFromRegulator    (md49_regulator,
+                              speed_1,
+                              speed_2);
 
-    std_msgs::Float64 actual_speed_1_msg; actual_speed_1_msg.data = actual_speed_1_m_per_s;
-    std_msgs::Float64 actual_speed_2_msg; actual_speed_2_msg.data = actual_speed_2_m_per_s;
+    setSpeedAtMotorController(md49,
+                              speed_1,
+                              speed_2);
 
-    actual_speed_1_publisher.publish(actual_speed_1_msg);
-    actual_speed_2_publisher.publish(actual_speed_2_msg);
+
+    publishActualSpeed       (actual_speed_1_publisher,
+                              actual_speed_1_m_per_s,
+                              actual_speed_2_publisher,
+                              actual_speed_2_m_per_s);
 
     ros::spinOnce();
 
@@ -97,6 +132,16 @@ int main(int argc, char **argv)
 /**************************************************************************************
  * FUNCTIONS
  **************************************************************************************/
+
+void speed_1_callback(std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator)
+{
+  md49_regulator.setSpeed1TargetValue(msg->data);
+}
+
+void speed_2_callback(std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator)
+{
+  md49_regulator.setSpeed2TargetValue(msg->data);
+}
 
 void getDeltaEncoderValues(MD49 &md49, uint32_t &delta_encoder_1, uint32_t &delta_encoder_2)
 {
@@ -115,12 +160,31 @@ void getDeltaEncoderValues(MD49 &md49, uint32_t &delta_encoder_1, uint32_t &delt
   current_encoder_2 = prev_encoder_2;
 }
 
-void speed_1_callback(std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator)
+void getActualSpeed_m_per_s(uint32_t const delta_encoder_1, uint32_t const delta_encoder_2,
+                            double &actual_speed_1_m_per_s, double &actual_speed_2_m_per_s)
 {
-  md49_regulator.setSpeed1TargetValue(msg->data);
+  actual_speed_1_m_per_s = EMG49::calcDistanceTraveled_m(delta_encoder_1, WHEEL_DIAMETER_m) / T_LOOP_UPDATE_s;
+  actual_speed_2_m_per_s = EMG49::calcDistanceTraveled_m(delta_encoder_2, WHEEL_DIAMETER_m) / T_LOOP_UPDATE_s;
 }
 
-void speed_2_callback(std_msgs::Float64::ConstPtr const & msg, MD49Regulator &md49_regulator)
+void getSpeedFromRegulator(MD49Regulator const &md49_regulator, int8_t &speed_1, int8_t &speed_2)
 {
-  md49_regulator.setSpeed2TargetValue(msg->data);
+  speed_1 = md49_regulator.getSpeed1();
+  speed_2 = md49_regulator.getSpeed2();
+}
+
+void setSpeedAtMotorController(MD49 &md49, int8_t const speed_1, int8_t const speed_2)
+{
+  md49.setSpeed1(speed_1);
+  md49.setSpeed2(speed_2);
+}
+
+void publishActualSpeed(ros::Publisher &actual_speed_1_publisher, double const actual_speed_1_m_per_s,
+                        ros::Publisher &actual_speed_2_publisher, double const actual_speed_2_m_per_s)
+{
+  std_msgs::Float64 actual_speed_1_msg; actual_speed_1_msg.data = actual_speed_1_m_per_s;
+  std_msgs::Float64 actual_speed_2_msg; actual_speed_2_msg.data = actual_speed_2_m_per_s;
+
+  actual_speed_1_publisher.publish(actual_speed_1_msg);
+  actual_speed_2_publisher.publish(actual_speed_2_msg);
 }
