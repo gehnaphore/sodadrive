f5008ed0fc065394f7090a4b066f6fac28c0808b
Alexander Entinger <cto@lxrobotics.com>
2016-08-31 06:34:15 +0200

    Improving anti windup protection

--- src/wheel_controller/PIDRegulator.cpp
+++ src/wheel_controller/PIDRegulator.cpp
@@ -30,26 +30,38 @@ PIDRegulator::PIDRegulator(double const kP, double const kI, double const kD, do
 
 double PIDRegulator::calc(double const target_value, double const actual_value)
 {
-  double const error = target_value - actual_value;
+  double const error      = target_value - actual_value;
 
-  double const p_out = _kP * error;
+  /* P */
 
-  double const error_sum = _error_sum + error * _dt_s;
-  if(error_sum >= _min && error_sum <= _max)
+  double const p_out      = _kP * error;
+
+  /* I */
+
+  double const error_sum  = _error_sum + error * _dt_s;
+  double       i_out      = _kI * error_sum;
+
+  if(i_out >= _min && i_out <= _max)
   {
     _error_sum = error_sum;
   }
-  
-  double const i_out = _kI * _error_sum;
+  else
+  {
+    i_out = std::max<double>(i_out, _min);
+    i_out = std::min<double>(i_out, _max);
+  }
 
-  double const d_out = _kD * (_error_prev - error);
+  /* D */
 
-  double out = p_out + i_out + d_out;
+  double const d_out      = _kD * (_error_prev - error);
+  _error_prev             = error;
+
+  /* Limit regulator output */
+
+  double       out        = p_out + i_out + d_out;
 
   out = std::max<double>(out, _min);
   out = std::min<double>(out, _max);
-
-  _error_prev = error;
 
   return out;
 }
